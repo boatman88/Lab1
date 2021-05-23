d4de2f56ebcca09565ad4ef332bb404088dd2a3a
Boatman88 <seanboatman88@gmail.com>
2021-05-23 10:20:00 -0400

    removed comments

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -63,58 +63,7 @@ namespace Deliverable1
 
                 }
             } while (tryAgain == true);
-                
-           
-            /*do { //statement; 
-             * } while( condition );
 
-
-            /* if (number % 2 == 0) { 
-            
-            Console.WriteLine("Number is Even"); 
-        
-        } else {
-            
-            Console.WriteLine("Number is Odd");
-
-        } Console.ReadLine();*/
         }
     }
 }
-/*Deliverable 1 Task: Oh no! A millennial hipster went back in time and changed all the units
-of measurement, giving the system a silly name, MilHip. Write a program that converts
-from MilHip to imperial measurements using the following table:
-
-Imperial    MilHip
-1 inch      3.5 fidget spinners
-1 foot      5 memes
-
-Task: Ask the user to input an amount and measurement type. Take the amount and parse
-it into a number variable that can handle decimals. Next check the input units and convert
-the amount based upon the table above. You only need to convert a unit to its equivalent
-in the other system (For example: change inches to fidget spinners, but not inches to Star
-Wars Rewrites).
-Then after doing the conversion, ask the user if they would like to do another one. If yes,
-loop and run through another conversion. If the user answers with anything else, exit the
-program.
-Rubric:
-● 1 pt: Ask the user to input a measurement type. Assume you will get valid inputs.
-● 1 pt: Ask the user for an amount. Assume valid inputs (such as the user won’t enter
-a string when a number is called for).
-● 1 pt: Convert and store that amount from a string into a number
-○ Hint: Use a data type that can handle decimals, as int cannot
-● Convert the entered number to the correct new unit
-○ 1 pt: inch value is converted properly to fidget spinners value
-○ 1 pt: fidget spinners value is converted properly to inch value
-○ 1 pt: foot value is converted properly to meme value
-○ 1 pt: meme value is converted properly to foot value
-● 1 pt: Output the result of the conversion along with the correct units (for instance, if
-input is 1 inch, output is 3.5 fidget spinners)
-● Ask the user if they wish to do another calculation
-○ 1 pt: If the user responds yes, loop back to the top of the program
-○ 1 pt: If the user responds with anything else, end the program
-*/
-
-/*two lines
-            string input = Console.ReadLine();
-            decimal unit2 = decimal.Parse(input);*/
